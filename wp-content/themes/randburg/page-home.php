d370c008b123dfae76a1939d7a731ef4399308db
Dominik Winter <dominik.winter@sydev.de>
2016-05-11 14:57:12 +0200

    Added simple categories-grid

--- wp-content/themes/randburg/page-home.php
+++ wp-content/themes/randburg/page-home.php
@@ -4,10 +4,21 @@
 */
 
 get_header();
+
+$pages = get_pages(array('child_of' => get_the_ID()));
 ?>
 
-<div id="catories-grid">
-  
+<div id="categories-grid">
+  <ul>
+    <?php foreach ( $pages as $page ) : ?>
+        <li class="item">
+            <div class="header" style="background-image: url(<?php echo wp_get_attachment_url(get_post_thumbnail_id($page->ID)); ?>)">
+              <div class="accent"></div>
+            </div>
+            <span><?php echo apply_filters( 'the_title', $page->post_title, $page->ID ); ?></span>
+        </li>
+    <?php endforeach; ?>
+  </ul>
 </div>
 
 <?php get_footer(); ?>
