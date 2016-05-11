2f4202fdfe5349199d5f2947c4f7b4d56ec68dd0
slade243 <ingo.hotischeck@live.de>
2016-05-11 16:19:27 +0100

    categories entfernt

--- wp-content/themes/randburg/page-home.php
+++ wp-content/themes/randburg/page-home.php
@@ -10,19 +10,8 @@ $pages = get_pages(array('exclude' => get_the_ID()));
 <div class="slideshow">
 	<?php do_action('slideshow_deploy', '27'); ?>
 </div>
+<div class="content">
+</div>
 
 
-<!--  <div id="categories-grid">
-  <ul>
-    <?php foreach ( $pages as $page ) : ?>
-        <li class="item">
-            <div class="header" style="background-image: url(<?php echo wp_get_attachment_url(get_post_thumbnail_id($page->ID)); ?>)">
-              <div class="accent"></div>
-            </div>
-            <span><?php echo apply_filters( 'the_title', $page->post_title, $page->ID ); ?></span>
-        </li>
-    <?php endforeach; ?>
-  </ul>
-</div> 
- -->
 <?php get_footer(); ?>
