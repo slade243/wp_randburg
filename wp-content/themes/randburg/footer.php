d8b73dcb5af43ea8b6541a8cba212839f7bfdfed
slade243 <ingo.hotischeck@live.de>
2016-05-12 16:41:51 +0100

    put the logos in the footer and several css customizations

--- wp-content/themes/randburg/footer.php
+++ wp-content/themes/randburg/footer.php
@@ -1,5 +1,12 @@
 <div id="footer">
-<h1>FOOTER</h1>
+	<div id="logoBox">
+		<img src="http://192.168.20.153/wp_randburg/wp-includes/images/certificates/construction_line.png" alt="Custom image" class="customImage" />
+		<img src="http://192.168.20.153/wp_randburg/wp-includes/images/certificates/niceic_approved_contractor.png" alt="Custom image" class="customImage" />
+		<img src="http://192.168.20.153/wp_randburg/wp-includes/images/certificates/niceic_domestic_installer.png" alt="Custom image" class="customImage" />
+		<img src="http://192.168.20.153/wp_randburg/wp-includes/images/certificates/has.png" alt="Custom image" class="customImage" />
+		<img src="http://192.168.20.153/wp_randburg/wp-includes/images/certificates/trust_mark.png" alt="Custom image" class="customImage" />
+		<img src="http://192.168.20.153/wp_randburg/wp-includes/images/certificates/ISO9001Registered.png" alt="Custom image" class="QMS" />
+	</div>
 </div>
 </div>
 </body>
--- wp-content/themes/randburg/page-home.php
+++ wp-content/themes/randburg/page-home.php
@@ -4,8 +4,8 @@
 */
 
 get_header();
-
-$pages = get_pages(array('exclude' => get_the_ID()));
+/* $pages = get_pages(array('exclude' => get_the_ID())); */
+$pages = get_pages(array('child_of' => '61'));
 ?>
 <div class="slideshow">
 	<?php do_action('slideshow_deploy', '27'); ?>
