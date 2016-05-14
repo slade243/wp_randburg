38ad66b9a987a41f7b0e68dda05afd32904b7a14
Dominik Winter <dominik.winter@sydev.de>
2016-05-14 20:28:55 +0200

    removed merge conflicts stuff

--- wp-content/themes/randburg/page-home.php
+++ wp-content/themes/randburg/page-home.php
@@ -5,18 +5,16 @@
 
 	get_header();
 ?>
-<<<<<<< HEAD
+
 <?php get_template_part('slider'); ?>
-=======
 <div class="slideshow">
 </div>
->>>>>>> origin/master
 <div class="content">
 	<h3>Services</h3>
 	<?php get_template_part('category', 'grid'); ?>
 </div>
 
 <?php
-	get_template_part('static', 'twitter'); 
-	get_footer(); 
+	get_template_part('static', 'twitter');
+	get_footer();
 ?>
