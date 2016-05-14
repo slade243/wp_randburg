aeac7f6599f8b4544cb37fb6050c470dd3a77b58
Dominik Winter <dominik.winter@sydev.de>
2016-05-14 20:32:35 +0200

    Added commentary for closing tags

--- wp-content/themes/randburg/footer.php
+++ wp-content/themes/randburg/footer.php
@@ -1,14 +1,14 @@
-<div id="footer">
-	<div id="logoBox">
-		<img src="<?php echo get_template_directory_uri() .'/images/certificates/construction_line.png'; ?>" alt="Custom image" class="customImage" />
-		<img src="<?php echo get_template_directory_uri() .'/images/certificates/niceic_approved_contractor.png'; ?>" alt="Custom image" class="customImage" />
-		<img src="<?php echo get_template_directory_uri() .'/images/certificates/niceic_domestic_installer.png'; ?>" alt="Custom image" class="customImage" />
-		<img src="<?php echo get_template_directory_uri() .'/images/certificates/has.png'; ?>" alt="Custom image" class="customImage" />
-		<img src="<?php echo get_template_directory_uri() .'/images/certificates/trust_mark.png'; ?>" alt="Custom image" class="customImage" />
-		<img src="<?php echo get_template_directory_uri() .'/images/certificates/ISO9001Registered.png'; ?>" alt="Custom image" class="QMS" />
+	<div id="footer">
+		<div id="logoBox">
+			<img src="<?php echo get_template_directory_uri() .'/images/certificates/construction_line.png'; ?>" alt="Custom image" class="customImage" />
+			<img src="<?php echo get_template_directory_uri() .'/images/certificates/niceic_approved_contractor.png'; ?>" alt="Custom image" class="customImage" />
+			<img src="<?php echo get_template_directory_uri() .'/images/certificates/niceic_domestic_installer.png'; ?>" alt="Custom image" class="customImage" />
+			<img src="<?php echo get_template_directory_uri() .'/images/certificates/has.png'; ?>" alt="Custom image" class="customImage" />
+			<img src="<?php echo get_template_directory_uri() .'/images/certificates/trust_mark.png'; ?>" alt="Custom image" class="customImage" />
+			<img src="<?php echo get_template_directory_uri() .'/images/certificates/ISO9001Registered.png'; ?>" alt="Custom image" class="QMS" />
+		</div>
 	</div>
-</div>
-</div>
+</div><!-- #wrapper -->
 
 <?php wp_footer(); ?>
 </body>
