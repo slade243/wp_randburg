91b4930401bd883c732676360be8d51d0d6e3ad6
Dominik Winter <dominik.winter@sydev.de>
2016-05-14 15:44:21 +0200

    Change for errors

--- wp-content/themes/randburg/page-home.php
+++ wp-content/themes/randburg/page-home.php
@@ -27,5 +27,18 @@ $pages = get_pages(array('child_of' => '61'));
 		</ul>
 	</div> 
 </div>
-<?php get_template_part(page, bottomBody); ?>
+
+<div id="categories-grid">
+  <ul>
+    <?php foreach ( $pages as $page ) : ?>
+        <li class="item">
+            <div class="header" style="background-image: url(<?php echo wp_get_attachment_url(get_post_thumbnail_id($page->ID)); ?>)">
+              <div class="accent"></div>
+            </div>
+            <span><?php echo apply_filters( 'the_title', $page->post_title, $page->ID ); ?></span>
+        </li>
+    <?php endforeach; ?>
+  </ul>
+</div>
+
 <?php get_footer(); ?>
