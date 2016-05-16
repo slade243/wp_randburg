672ce228858bf8d830cc0edda16ee3af8892fd86
Dominik Winter <dominik.winter@sydev.de>
2016-05-16 18:23:51 +0200

    Hover effects for grid

--- wp-content/themes/randburg/category-grid.php
+++ wp-content/themes/randburg/category-grid.php
@@ -11,8 +11,12 @@
 		<?php foreach ( $pages as $page ) : ?>
 			<li class="item">
 				<a href="<?php echo get_page_link($page->ID); ?>">
-					<div class="category-header" style="background-image: url(<?php echo wp_get_attachment_url(get_post_thumbnail_id($page->ID)); ?>)"></div>
-					<span class="dash"><span><?php echo apply_filters( 'the_title', $page->post_title, $page->ID ); ?></span></span>
+					<div class="category-image" style="background-image: url(<?php echo wp_get_attachment_url(get_post_thumbnail_id($page->ID)); ?>)"></div>
+					<div class="category-content">
+						<h3><?php echo $page->post_title; ?></h3>
+						<p>Id mollit voluptate aliquip</p>
+					</div>
+					<div class="category-overlay"></div>
 				</a>
 			</li>
 		<?php endforeach; ?>
