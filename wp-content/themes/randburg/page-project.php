cc3045f1bb56da5ef6b4ef4ec03715655bea0789
Zaybot <t.schnellenbach@gmail.com>
2016-05-19 13:31:46 +0100

    adjustes page-project.php

--- wp-content/themes/randburg/page-project.php
+++ wp-content/themes/randburg/page-project.php
@@ -1,7 +1,23 @@
 <?php
-	get_header();
-	get_template_part('banner', 'featured');
+/**
+* Template Name: Project
+*/
+
+  get_header();
 ?>
+
+<div class="content">
+
+	<?php
+      while ( have_posts() ) : the_post();
+        ?>
+        <div class="page-content">
+          <?php the_content(); ?>
+        </div>
+        <?php
+      endwhile;
+      ?>
+</div><!-- .content-area -->
 
 <?php
 	get_template_part('static', 'twitter');
