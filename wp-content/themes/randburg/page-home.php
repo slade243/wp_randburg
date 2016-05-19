a3dbf58336ca145e2317b1f403ffd2108f6cba36
slade243 <ingo.hotischeck@live.de>
2016-05-19 14:08:34 +0100

    inserted post in page-home and aligned text

--- wp-content/themes/randburg/page-home.php
+++ wp-content/themes/randburg/page-home.php
@@ -7,6 +7,16 @@
 	get_template_part('slider');
 ?>
 <div class="content">
+	<h1>Randburg electrical services</h1>
+	<?php
+		while ( have_posts() ) : the_post();
+    ?>
+    <div class="page-content">
+		<?php the_content(); ?>
+    </div>
+    <?php
+		endwhile;
+	?>
 	<h2 class="headline">Services</h2>
 	<?php get_template_part('category', 'grid'); ?>
 </div>
