32de7379afeadbb18611b4eb1b2fe443c51e3a58
Dominik Winter <dominik.winter@sydev.de>
2016-05-19 16:31:33 +0200

    Updated Category Grid

--- wp-content/themes/randburg/category-grid.php
+++ wp-content/themes/randburg/category-grid.php
@@ -11,7 +11,7 @@
 		<?php foreach ( $pages as $page ) : ?>
 			<li class="item">
 				<a href="<?php echo get_page_link($page->ID); ?>">
-					<div class="category-image" style="background-image: url(<?php echo wp_get_attachment_url(get_post_thumbnail_id($page->ID)); ?>)"></div>
+					<div class="category-image" style="background-image: url(<?php echo wp_get_attachment_image_src(get_post_thumbnail_id($page->ID), 'medium')[0]; ?>)"></div>
 					<div class="category-content">
 						<h3><?php echo $page->post_title; ?></h3>
 						<p><?php echo get_post_meta($page->ID, '_featured_text', true); ?></p>
